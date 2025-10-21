7d98e2ec9b6463bc525f05207a06488d09c3abb7
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-10-21 15:43:41 +0300

    Added comments

--- destinova/src/Components/Destinations.jsx
+++ destinova/src/Components/Destinations.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
+// Import DestinationCard
 import DestinationCard from "./DestinationCard";
 
 function Destinations() {
+    // Sets the useState to empty array for destinations, true to loading and empty strings to the search input field.
   const [destinations, setDestinations] = useState([]);
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState("");
-
+// UseEffect and data fetching from db.json file
   useEffect(() => {
     fetch("http://localhost:3000/destinations")
       .then((res) => res.json())
@@ -16,32 +18,44 @@ function Destinations() {
       .catch((error) => console.error("Error fetching destinations:", error));
   }, []);
 
-  // Filter destinations by name
+  // Filter destinations by name 
   const filteredDestinations = destinations.filter((d) =>
     d.name.toLowerCase().includes(search.toLowerCase())
   );
-
+//  THis message is displayed when the page is loading.
   if (loading) {
     return <p>Loading Destinations...</p>;
   }
 
   return (
-      <>
-          <h1 style={{textAlign: "center", wordSpacing: "2"}}>Destinations</h1>
-      <h3>Explore Destinations</h3>
+    <>
+      <h1 style={{ textAlign: "center" }}>Destinations</h1>
+      <h3 style={{ marginLeft: "0" }}>Explore Destinations</h3>
       <input
         type="text"
         placeholder="Search destinations by name"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-          />
-
-      <div className="destination-grid" style={{display: "grid",  gridTemplateColumns: "repeat(3, 1fr)",gap:"1.5rem", padding: "2rems", justifyItems: "center", margin: "15px"}}>
+      />
+{/* This section is displayed immediately the page loads and it gets filtered when a user searches a destination  */}
+      <div
+              className="destination-grid"
+            //   added some inline styling to ensure only 3 destinatios are displayed per line.
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gap: "1.5rem",
+          padding: "2rems",
+          justifyItems: "center",
+          margin: "15px",
+        }}
+      >
         {filteredDestinations.length > 0 ? (
           filteredDestinations.map((dest) => (
             <DestinationCard key={dest.id} {...dest} />
           ))
-        ) : (
+              ) : (
+                    //   This message pops up when no destination with the search name is found
           <p>No destinations found with that name.</p>
         )}
       </div>
