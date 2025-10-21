155fc57d23e222b6868298c0a317e89ac70b1e23
bradley <fazebrad13@gmail.com>
2025-10-21 21:13:57 +0300

    Fixed the NavBar and rendering issues

--- destinova/src/App.jsx
+++ destinova/src/App.jsx
@@ -1,11 +1,14 @@
-
+import Navbar from './Components/Navbar'
 import React from 'react'
 import Destinations from './Components/Destinations'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Destinations />
     </div>
   )
 }
+
+export default App
