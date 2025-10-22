d701ab0baf6b2f92cf0c1612db30d0cd01357fec
stanley <sgbarasa@gmail.com>
2025-10-22 14:38:30 +0300

    configured routes and added <link> in navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import Navbar from './Components/Navbar'
-import React from 'react'
-import Destinations from './Components/Destinations'
-
+import React from "react";
+import Navbar from "./Components/Navbar.jsx";
+import Destinations from "./pages/Destinations.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <Destinations />
-    </div>
-  )
+      <Routes>
+        <Route path="/" element={<h1>Welcome to Destinova</h1>} />
+        <Route path="/destinations" element={<Destinations />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
