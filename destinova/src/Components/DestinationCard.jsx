a36749b82688875295b0368b3daea860cc860b46
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-10-22 11:05:32 +0300

    Removed a style

--- destinova/src/Components/DestinationCard.jsx
+++ destinova/src/Components/DestinationCard.jsx
@@ -1,29 +1,25 @@
 import React from "react";
 
-function DestinationCard({ name, description, location, price, rating, image }) {
-
-
+function DestinationCard({ name, location, price, rating, image }) {
   return (
     <div className="bg-white rounded-xl shadow-md hover:shadow-lg transition duration-300 overflow-hidden w-full max-w-sm">
+      
       {/* Image */}
-      <img src={image} alt={name} className="w-full h-48 object-cover" />
+      <img
+        src={image}
+        alt={name}
+        className="w-full h-48 object-cover"
+      />
       {/* Content */}
       <div className="p-4 text-center">
         <h2 className="text-lg font-semibold text-gray-800">{name}</h2>
-        <p className="text-sm text-gray-700">{description }</p>
         <p className="text-sm text-gray-500">{location}</p>
-
-        {/* Price and Rating */}
+      
+      {/* Price and Rating */}
         <div className="flex justify-center items-center gap-3 mt-2 text-sm">
           <p className="font-medium text-gray-700">💲{price}</p>
           <p className="text-yellow-500">⭐ {rating}</p>
         </div>
-        {/* <button
-          className="like-button"
-          onClick={() => onAddToBucket({ name, image, description, location })}
-        >
-          ❤️ Add to Bucket List
-        </button> */}
       </div>
     </div>
   );
