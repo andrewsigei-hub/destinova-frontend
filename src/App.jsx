c6028529b486e503a3d1aae74d6613ccbc512f99
bradley <fazebrad13@gmail.com>
2025-10-22 13:00:11 +0300

    configured toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import Navbar from './Components/Navbar'
 import React from 'react'
 import Destinations from './Components/Destinations'
-
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
+  const notify = () => toast("Wow so easy!");
   return (
     <div>
       <Navbar />
       <Destinations />
+      <button onClick={notify}>Notify!</button>
+        <ToastContainer />
     </div>
+    
   )
 }
 
