98419d8f36cae67f418923583339fee2374d04ed
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-10-27 08:13:37 +0300

    Added a new description

--- src/Components/DestinationCard.jsx
+++ src/Components/DestinationCard.jsx
@@ -3,19 +3,14 @@ import React from "react";
 function DestinationCard({ name, location, price, rating, image }) {
   return (
     <div className="bg-white rounded-xl shadow-md hover:shadow-lg transition duration-300 overflow-hidden w-full max-w-sm">
-      
       {/* Image */}
-      <img
-        src={image}
-        alt={name}
-        className="w-full h-48 object-cover"
-      />
+      <img src={image} alt={name} className="w-full h-48 object-cover" />
       {/* Content */}
       <div className="p-4 text-center">
         <h2 className="text-lg font-semibold text-gray-800">{name}</h2>
+
+        {/* Price, location, description  and Rating */}
         <p className="text-sm text-gray-500">{location}</p>
-      
-      {/* Price and Rating */}
         <div className="flex justify-center items-center gap-3 mt-2 text-sm">
           <p className="font-medium text-gray-700">💲{price}</p>
           <p className="text-yellow-500">⭐ {rating}</p>
